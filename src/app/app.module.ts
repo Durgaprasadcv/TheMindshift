1f23093b83b87a7136ec8dfe6966858b2f51f6e3
DP-Zinc <durga@zincdigital.com>
2017-08-06 18:25:14 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import { MaterialModule } from '@angular/material';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule, 
-    MdCheckboxModule
+    MdCheckboxModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    RouterModule.forRoot([
+      { path: 'login', component:LoginComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
