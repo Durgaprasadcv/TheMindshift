68764d4c69b64900f8ac69be027470d052473a42
sowmyadjit <soumidas.km@gmail.com>
2017-09-02 15:02:02 +0530

    chapter bug fix

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{ Http , Response}from'@angular/http'
 import 'assets/te1.js'
+import { Router } from '@angular/router';
 declare var webGlObject1: any;
 
 @Component({
@@ -11,11 +12,13 @@ declare var webGlObject1: any;
 export class ChapterComponent implements OnInit {
   public returnmsg;
   public returnmsg_menu;
-  constructor(private http:Http) {
-    webGlObject1.init();
+  public initc = false;
+  constructor(private http:Http,private _router: Router) {
+    //webGlObject1.init();
+    this.initc = false;
    }
   ngOnInit() : void {
-    webGlObject1.init();
+    //webGlObject1.init();
     this.http.get('assets/json/product1.json')
     .subscribe(
           data => this.returnmsg = data.json(),
@@ -27,4 +30,13 @@ export class ChapterComponent implements OnInit {
           err => console.log('failed'),
           () => console.log('Success Return data:',this.returnmsg_menu));
  }
+
+ initcarousel(t) :void {
+  
+    if(t && !this.initc)
+     {
+       webGlObject1.init();
+       this.initc = true;
+     }
+  }
 }
